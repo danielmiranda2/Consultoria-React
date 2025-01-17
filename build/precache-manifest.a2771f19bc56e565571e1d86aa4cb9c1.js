13b07ba2dc8170fa5b1e39f995af95700cc49127
danielmiranda-tech <daniel.miranda@netconamericas.com>
2025-01-17 14:33:41 -0300

    Consultoria

---
+++ build/precache-manifest.a2771f19bc56e565571e1d86aa4cb9c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb2c8752c1c711701d54f79890026a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12422785b1c6052f4982",
+    "url": "/static/css/main.0f04486e.chunk.css"
+  },
+  {
+    "revision": "d177aee21ce4e2708128",
+    "url": "/static/js/2.e623f0c2.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.e623f0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12422785b1c6052f4982",
+    "url": "/static/js/main.6f25667c.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
